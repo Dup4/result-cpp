0666e543a8ae6159508cdf392a428deeaa582341
Dup4 <lyuzhi.pan@gmail.com>
2022-07-09 13:18:48 +0800

    refactor: split macros_impl file

---
+++ include/result/macros_impl.h
@@ -0,0 +1,21 @@
+#ifndef RESULT_MACROS_IMPL_H
+#define RESULT_MACROS_IMPL_H
+
+#define __RESULT_OR_RETURN_IMPL(func, res_name) \
+    auto res_name = func;                       \
+    if (!res_name.IsOK()) {                     \
+        return res_name;                        \
+    }
+
+#define __RESULT_VALUE_OR_RETURN_IMPL(var_name, func, res_name) \
+    auto res_name = func;                                       \
+    if (!res_name.IsOK()) {                                     \
+        return res_name;                                        \
+    }                                                           \
+                                                                \
+    auto var_name = std::move(res_name.Value());
+
+#define __RESULT_GENERATE_RES_NAME(line, counter) __RESULT_GENERATE_RES_NAME_(line, counter)
+#define __RESULT_GENERATE_RES_NAME_(line, counter) __res_##line##_##counter##_
+
+#endif  // RESULT_MACROS_IMPL_H
