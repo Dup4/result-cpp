49c7ca0739fb6a7e600d2fa9e45140d5ae2863d8
Dup4 <lyuzhi.pan@gmail.com>
2022-07-12 23:27:08 +0800

    test: add ResultToResult and ResultOrToResultOr benchmark

--- test/result_benchmark.cc
+++ test/result_benchmark.cc
@@ -4,6 +4,17 @@
 
 using namespace custom_result;
 
+static void BenchmarkResultToResult(benchmark::State& state) {
+    auto f = []() -> CustomResult {
+        auto res = CustomResult::Builder(CustomResult::ErrorCode::OK).Build();
+        return res;
+    };
+
+    for (auto _ : state) {
+        auto res = f();
+    }
+}
+
 static void BenchmarkResultToResultOr(benchmark::State& state) {
     auto f = []() -> CustomResultOr<int> {
         auto res = CustomResult::Builder(CustomResult::ErrorCode::OK).Build();
@@ -26,5 +37,18 @@ static void BenchmarkResultOrToResult(benchmark::State& state) {
     }
 }
 
+static void BenchmarkResultOrToResultOr(benchmark::State& state) {
+    auto f = []() -> CustomResultOr<int> {
+        CustomResultOr<int> a = 1;
+        return a;
+    };
+
+    for (auto _ : state) {
+        auto res = f();
+    }
+}
+
+BENCHMARK(BenchmarkResultToResult);
 BENCHMARK(BenchmarkResultToResultOr);
 BENCHMARK(BenchmarkResultOrToResult);
+BENCHMARK(BenchmarkResultOrToResultOr);
